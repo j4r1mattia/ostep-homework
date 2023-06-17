854c96d971a0ee705f8d8d540d53aec8b128f35d
j4r1 <j4r1.dev@gmail.com>
2023-06-17 18:42:59 +0200

    add hw chapter 2

--- cpu-api/hw3.c
+++ cpu-api/hw3.c
@@ -6,7 +6,22 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/time.h>
 
+long getMillis() {
+  struct timeval time;
+  int rc = gettimeofday(&time, NULL);
+  assert(rc > -1);
+  return time.tv_usec;
+}
+
+int executeWithDuration(int (*f)()){
+  long before = getMillis();
+  int rc = f();
+  long after = getMillis();
+  printf("Duration is %ld ms\n", (after - before));
+  return rc;
+}
 
 void wait_or_die() {
   int rc = wait(NULL);
@@ -21,11 +36,12 @@ int fork_or_die() {
 
 int main() {
 
-  if(fork_or_die() == 0) {
+
+  if(executeWithDuration(fork_or_die) == 0) {
     printf("hello ");
     exit(0);
   }
-  
+
   printf("goodbye");
   wait_or_die();
   return 0;
